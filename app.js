d48f7a0f2dff58a22dc4792f79849b6aa5ae970b
v1nnii <zheneskudavid100@mail.ru>
2022-12-10 23:19:52 +0300

    добавилbootstrap

--- app.js
+++ app.js
@@ -36,7 +36,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',
+  {
+    title: 'Ошибочка вышла ;('
+  });
 });
 
 module.exports = app;
