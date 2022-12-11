9a40054d56ae847a559e6507fb4b87468341fd64
v1nnii <zheneskudavid100@mail.ru>
2022-12-11 12:08:40 +0300

    создал монго базу

---
+++ createDB.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient
+
+const uri = "mongodb://127.0.0.1:27017/"
+const client = new MongoClient(uri)
+async function run() {
+try {
+await client.connect();
+var database = client.db("milana_h");
+database.dropDatabase()
+database = client.db("milana_h");
+const muzk = database.collection("muzk");
+const result = await muzk.insertOne({name:"Хаметова"});
+console.log(`${result} documents were inserted`);
+} finally {
+await client.close();
+}
+}
+run()
